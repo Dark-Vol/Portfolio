f3a068150c613a01ec1e5feb15c7f829980c24c4
Dark-Vol <overlord28985@gmail.com>
2025-02-21 15:19:38 +0200

    file about me

---
+++ data/personal-data.js
@@ -0,0 +1,27 @@
+export const personalData = {
+  name: 'Nikita Vavilov',
+  websiteLogo: 'website logo.png',
+  mainSkills: ['Front-end', "Full-stack", 'Back-end Developer'],
+  profilePic: 'profile-pic.png', // Add your photo to the "images" folder and specify its filename here
+  bio: `I am a passionate Front-End developer with over 2 years of hands-on experience building dynamic and user-friendly web applications. Recently, I have started to expand my skills by diving into Backend development, aiming to build end-to-end solutions. During my time at CloverPlate (March 27, 2024 to August 27, 2024), I honed my knowledge of front-end technologies by collaborating on important projects and working in a fast-paced environment. Now, I am excited to leverage my skills in both Front-End and Backend to contribute to innovative and challenging projects.`,
+  resume: 'meowish resume.pdf', // Add your PDF file to the "data" folder and specify its filename here
+  techStacks: [
+    // Add your tech stack icons to the "icons" folder and specify their filenames here
+    { name: 'HTML', icon: 'html icon.png' },
+    { name: 'CSS', icon: 'css-3 icon.png' },
+    { name: 'JavaScript', icon: 'js icon.png' },
+    { name: 'ExpressJS', icon: 'expressJS icon.png' },
+    { name: 'NodeJS', icon: 'nodeJS icon.png' },
+    { name: 'Sass', icon: 'sass.svg' },
+    { name: 'Figma', icon: 'figma icon.png' },
+    { name: 'React', icon: 'react icon.png' },
+    { name: 'Git', icon: 'git icon.png' },
+    { name: 'MySql', icon: 'MySql.png'},
+    { name: 'Xampp', icon: 'xampp-logo-ac950edf.svg'},
+  ],
+  contactNumber: '+380505799437',
+  email: 'overlord28985@gmail.com',
+  tiktokLink: 'https://www.tiktok.com/ExampleTiktokLink', // Open your TikTok profile in a browser and copy the link here
+  githubLink: 'https://github.com/Dark-Vol', // Replace with your GitHub profile link
+  facebookLink: 'https://www.facebook.com/exampleFacebookLink.6/' // Open your Facebook profile in a browser and copy the link here
+}
