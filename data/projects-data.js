0d90d37080f87309f7589363821f3817df8d8df0
Dark-Vol <overlord28985@gmail.com>
2025-04-01 14:12:28 +0300

    refactor project data structure and update project overviews

--- data/projects-data.js
+++ data/projects-data.js
@@ -1,14 +1,14 @@
 
 const projects = [
   {
-    id: 0, // Don't change the project ID
+    id: 0,
     title: 'Hard Portfolio',
     mainColor: '#00ADB5',
     type: 'Website',
     yearCreated: '2025',
     projectGoal: 'Presenting my work, skills and personal info.',
-    logo: 'project logo 1.png', // Add your project logo to the "projects-logos" folder and specify the filename here
-    image: 'project-image1.png', // Add your project image to the "images" folder and specify the filename here
+    logo: 'project logo 1.png',
+    image: 'project-image1.png',
     projectLink: '',
     projectLinkGit: "https://github.com/Dark-Vol/Store-Shoping",
     overview: `This project is being created as a diploma project in the specialty "Software Engineering".`,
@@ -62,11 +62,11 @@ const projects = [
     type: 'Website',
     yearCreated: '2025',
     projectGoal: 'The project is a virtual organic food store application with a fake json server database (REST API) React.js <-> Node.js',
-    logo: 'project logo 2.png', // Add your project logo to the "projects-logos" folder and specify the filename here
-    image: '68747470733a2f2f692e6962622e636f2f683154447933622f42616e6e65722e6a7067.png', // Add your project image to the "images" folder and specify the filename here
+    logo: 'project logo 2.png',
+    image: '68747470733a2f2f692e6962622e636f2f683154447933622f42616e6e65722e6a7067.png',
     projectLink: '',
     projectLinkGit: "https://github.com/Dark-Vol/Organick",
-    overview: `Under development.`,
+    overview: `Project completed. The project is a virtual organic food store application with a fake json server database (REST API) React.js <-> Node.js`,
     techStacks: [
       {
         name: 'Sass',
@@ -96,14 +96,14 @@ const projects = [
     ]
   },
   {
-    id: 2, // Don't change the project ID
+    id: 2,
     title: 'Odigo',
     mainColor: '#e6c200',
     type: 'Website',
     yearCreated: '2025',
     projectGoal: 'Presenting my work, skills and personal info.',
-    logo: 'project logo 3.png', // Add your project logo to the "projects-logos" folder and specify the filename here
-    image: 'Odigo.png', // Add your project image to the "images" folder and specify the filename here
+    logo: 'project logo 3.png',
+    image: 'Odigo.png',
     projectLink: 'https://dark-vol.github.io/Odigo/',
     projectLinkGit: "https://github.com/Dark-Vol/Odigo",
     overview: `Put your project overview here. Briefly describe what your project is about and its key features.`,
@@ -126,19 +126,18 @@ const projects = [
     ]
   },
   {
-    id: 3, // Don't change the project ID
+    id: 3,
     title: 'Createx-Marathon',
     mainColor: 'yellow',
     type: 'Website',
     yearCreated: '2025',
     projectGoal: 'Presenting my work, skills and personal info.',
-    logo: 'project logo 4.png', // Add your project logo to the "projects-logos" folder and specify the filename here
-    image: 'Createx-Marathon.png', // Add your project image to the "images" folder and specify the filename here
+    logo: 'project logo 4.png',
+    image: 'Createx-Marathon.png',
     projectLink: 'https://dark-vol.github.io/Createx-Marathon/',
     projectLinkGit: "https://github.com/Dark-Vol/Createx-Marathon",
     overview: `Put your project overview here. Briefly describe what your project is about and its key features.`,
     techStacks: [
-      // Add your tech stack icons to the "icons" folder and specify their filenames here
       {
         name: 'HTML',
         type: 'HyperText Markup Language',
@@ -168,13 +167,12 @@ const projects = [
     type: '',
     yearCreated: '2024',
     projectGoal: 'To display some of my information',
-    logo: 'project logo 5.png', // Add your project logo to the "projects-logos" folder and specify the filename here
-    image: 'project-image5.png', // Add your project image to the "images" folder and specify the filename here
+    logo: 'project logo 5.png',
+    image: 'project-image5.png',
     projectLink: '',
     projectLinkGit: "",
     overview: `Put your project overview here. Briefly describe what your project is about and its key features.`,
     techStacks: [
-      // Add your tech stack icons to the "icons" folder and specify their filenames here
       {
         name: 'X',
         type: 'X',
@@ -182,25 +180,25 @@ const projects = [
       }
     ]
   },
-  {
-    id: 4, // Don't change the project ID
-    title: 'Astro Solve',
-    mainColor: 'rgb(111, 0, 202)',
-    type: 'Xn',
-    yearCreated: '2027',
-    projectGoal: 'To display some of my information',
-    logo: 'project logo 6.png', // Add your project logo to the "projects-logos" folder and specify the filename here
-    image: 'project-image6.png', // Add your project image to the "images" folder and specify the filename here
-    projectLink: '#',
-    overview: `Put your project overview here. Briefly describe what your project is about and its key features.`,
-    techStacks: [
-      {
-        name: 'X',
-        type: 'X',
-        icon: 'X',      
-      }
-    ]
-  }
+  // {
+  //   id: 4, // Don't change the project ID
+  //   title: 'Astro Solve',
+  //   mainColor: 'rgb(111, 0, 202)',
+  //   type: 'Xn',
+  //   yearCreated: '2027',
+  //   projectGoal: 'To display some of my information',
+  //   logo: 'project logo 6.png',
+  //   image: 'project-image6.png',
+  //   projectLink: '#',
+  //   overview: `Put your project overview here. Briefly describe what your project is about and its key features.`,
+  //   techStacks: [
+  //     {
+  //       name: 'X',
+  //       type: 'X',
+  //       icon: 'X',      
+  //     }
+  //   ]
+  // }
 ];
 
 
